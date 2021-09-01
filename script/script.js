78f80f39117ac8aa86825181a47bf73494d60d65
alessandroNessi <alessandronessi@msn.com>
2021-09-01 17:12:34 +0200

    added a select

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
-/**
- * 
- * funzione che dati il numero di celle crea una griglia con dimensione impostata dalle variabili css 
+/**given a percentage return with the same probability true or false */
+function perc(x){
+    if(Math.random()*100<x){
+        return true;
+    }else{
+        return false;
+    }
+}
+/** funzione che dati il numero di celle crea una griglia con dimensione impostata dalle variabili css 
  */
 function generateGameBoard(cells){
     var board=document.getElementById("gameFrame");
@@ -15,17 +21,34 @@ function generateGameBoard(cells){
     //write a var cellwidth in css that is set as sigle cell width
     root.style.setProperty("--cellWidth" , cellWidth);
     for(var i=0;i<(cells*cells);i++){
-        // board.innerHTML+=`<div class="cell stdBgr"><p class="invisible">${i}</p></div>`;
-        board.innerHTML+=`<div class="cell bomb"><div class="stdBgr"><p class="invisible">${i}</p></div></div>`;
+        //chanche of a bomb
+        if(perc(10)){
+            board.innerHTML+=`<div class="cell bomb"><div class="stdBgr"><p class="invisible">${i}</p></div></div>`;
+        }else{
+            board.innerHTML+=`<div class="cell"><div class="stdBgr"><p class="invisible">${i}</p></div></div>`;
+        }
     }
 }
-
-var cells=prompt("inserire il numero di celle <=10: ");
-if (cells<=10){
-    generateGameBoard(cells);
-}
+document.getElementById("choice").addEventListener("change", function(event){
+    let size=parseInt(document.getElementById("choice").value);
+    if(!isNaN(size)){
+        // alert(`choice ${size}`);
+        generateGameBoard(size);
+        document.getElementById("choice").parentElement.classList.add("invisible");
+    }else{
+        alert("invalid choice");
+    }
+});
+// var cells=prompt("inserire il numero di celle <=10: ");
+// if (cells<=10){
+//     generateGameBoard(cells);
+// }
 document.getElementById("gameFrame").addEventListener("click",function(event){
     console.log(event);
     event.target.classList.remove("stdBgr");
     event.target.getElementsByClassName("invisible")[0].classList.remove("invisible");
+    console.log(event.target.classList);
+    if(event.target.parentElement.classList.contains("bomb")){
+        alert("you lost!");
+    }
 });
